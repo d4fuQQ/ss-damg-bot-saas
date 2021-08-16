c76cc00e94e7ee09a3f50529b4c16b5dca96ab88
KyleStarkes <71524516+KyleStarkes@users.noreply.github.com>
2021-08-16 19:06:35 -0400

    Fix rank all

--- slp_bot.py
+++ slp_bot.py
@@ -140,6 +140,10 @@ def get_all_rank_msg():
     address_to_discord_id = get_address_to_discord_id_dict()
 
     for key, row in df.iterrows():
+        days_since_claimed = get_days_since_claimed(row['last_updated'], row['last_claimed'])
+        if days_since_claimed < 1:
+            continue
+            
         if row['address'] not in address_to_discord_id:
             continue
 
